fa1587c894b4ea3dd9fbaf2595fac3e11e9dd987
Gagandeep Singh <gagan@supergroup.ai>
2023-06-24 18:50:08 +0530

    backup of mongo using cron

--- k8/backer/mongo/entrypoint.sh
+++ k8/backer/mongo/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# Copied from https://github.com/birotaio/mongodb-gcs-backup
 
-# echo "sleeping"
-# sleep infinity
-# exit 1
+#echo "sleeping"
+#sleep infinity
+#exit 1
+
+set -x
+set -e
 
 set -o pipefail
 set -o errexit
@@ -10,54 +14,36 @@ set -o errtrace
 set -o nounset
 # set -o xtrace
 
+echo "Starting backup script"
+date
+
 BACKUP_DIR=${BACKUP_DIR:-/tmp}
 BOTO_CONFIG_PATH=${BOTO_CONFIG_PATH:-/root/.boto}
 GCS_BUCKET=${GCS_BUCKET:-}
 GCS_KEY_FILE_PATH=${GCS_KEY_FILE_PATH:-}
-MONGODB_HOST=${MONGODB_HOST:-localhost}
-MONGODB_PORT=${MONGODB_PORT:-27017}
+
+MONGODB_HOST=${MONGODB_HOST:-}
+MONGODB_PORT=${MONGODB_PORT:-}
 MONGODB_DB=${MONGODB_DB:-}
-MONGODB_USER=${MONGODB_USER:-}
-MONGODB_PASSWORD=${MONGODB_PASSWORD:-}
-MONGODB_OPLOG=${MONGODB_OPLOG:-}
-SLACK_ALERTS=${SLACK_ALERTS:-}
-SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL:-}
+
 SLACK_CHANNEL=${SLACK_CHANNEL:-}
-SLACK_USERNAME=${SLACK_USERNAME:-}
-SLACK_ICON=${SLACK_ICON:-}
+SLACK_TOKEN=${SLACK_TOKEN:-}
+
 
 backup() {
-  mkdir -p $BACKUP_DIR
-  date=$(date "+%Y-%m-%dT%H:%M:%SZ")
-  archive_name="mongo-backup-$date.tar.gz"
+    mkdir -p $BACKUP_DIR
+    date=$(date "+%Y-%m-%dT%H%M%SZ")
+    archive_name="$BACKUP_DIR/mongo-backup-$date.tar.gz"
 
-  cmd_auth_part=""
-  if [[ ! -z $MONGODB_USER ]] && [[ ! -z $MONGODB_PASSWORD ]]
-  then
-    cmd_auth_part="--username=\"$MONGODB_USER\" --password=\"$MONGODB_PASSWORD\""
-  fi
-
-  cmd_db_part=""
-  if [[ ! -z $MONGODB_DB ]]
-  then
-    cmd_db_part="--db=\"$MONGODB_DB\""
-  fi
-
-  cmd_oplog_part=""
-  if [[ $MONGODB_OPLOG = "true" ]]
-  then
-    cmd_oplog_part="--oplog"
-  fi
-
-  cmd="mongodump --host=\"$MONGODB_HOST\" --port=\"$MONGODB_PORT\" $cmd_auth_part $cmd_db_part $cmd_oplog_part --gzip --archive=$BACKUP_DIR/$archive_name"
-  echo "starting to backup MongoDB host=$MONGODB_HOST port=$MONGODB_PORT"
-  eval "$cmd"
+    cmd="mongodump --host=$MONGODB_HOST --port=$MONGODB_PORT --db=$MONGODB_DB --gzip --archive=$archive_name"
+    echo "starting to backup Mongo"
+    eval "$cmd"
 }
 
 upload_to_gcs() {
-  if [[ $GCS_KEY_FILE_PATH != "" ]]
-  then
-cat <<EOF > $BOTO_CONFIG_PATH
+    if [[ $GCS_KEY_FILE_PATH != "" ]]
+    then
+    cat <<EOF > $BOTO_CONFIG_PATH
 [Credentials]
 gs_service_key_file = $GCS_KEY_FILE_PATH
 [Boto]
@@ -68,45 +54,31 @@ content_language = en
 default_api_version = 2
 [OAuth2]
 EOF
-  fi
-  echo "uploading backup archive to GCS bucket=$GCS_BUCKET"
-  gsutil cp $BACKUP_DIR/$archive_name $GCS_BUCKET
+    fi
+    echo "uploading backup archive to GCS bucket=$GCS_BUCKET"
+    gsutil cp $archive_name $GCS_BUCKET
+    send_slack_message "Mongo backup done :smile: : $archive_name"
 }
 
 send_slack_message() {
-  local color=${1}
-  local title=${2}
-  local message=${3}
-
-  echo 'Sending to '${SLACK_CHANNEL}'...'
-  curl --silent --data-urlencode \
-    "$(printf 'payload={"channel": "%s", "username": "%s", "link_names": "true", "icon_emoji": "%s", "attachments": [{"author_name": "mongodb-gcs-backup", "title": "%s", "text": "%s", "color": "%s"}]}' \
-        "${SLACK_CHANNEL}" \
-        "${SLACK_USERNAME}" \
-        "${SLACK_ICON}" \
-        "${title}" \
-        "${message}" \
-        "${color}" \
-    )" \
-    ${SLACK_WEBHOOK_URL} || true
-  echo
+    local message=${1}
+    echo 'Sending to '${SLACK_CHANNEL}'...'
+    curl -X POST https://slack.com/api/chat.postMessage -H "Content-type: application/json" -H "Authorization: Bearer $SLACK_TOKEN" -d"{'channel': '$SLACK_CHANNEL', 'text': '$message'}"
+    echo
 }
 
 err() {
-  err_msg="Something went wrong on line $(caller)"
-  echo $err_msg >&2
-  if [[ $SLACK_ALERTS == "true" ]]
-  then
-    send_slack_message "danger" "Error while performing mongodb backup" "$err_msg"
-  fi
+    err_msg="Something went wrong on line $(caller)"
+    echo $err_msg >&2
+    send_slack_message "Error while performing mongo backup :cry: : $err_msg"
 }
 
 cleanup() {
-  rm $BACKUP_DIR/$archive_name
+    rm $archive_name
 }
 
 trap err ERR
 backup
 upload_to_gcs
 cleanup
-echo "backup done!"
+echo "mongo backup done!"
