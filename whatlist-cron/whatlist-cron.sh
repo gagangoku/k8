ded56d78d1dfd0bfdc06f55f9889e48a4ca3534a
Gagandeep Singh <gagan@supergroup.ai>
2024-03-10 22:07:03 +0530

    run twice

--- whatlist-cron/whatlist-cron.sh
+++ whatlist-cron/whatlist-cron.sh
@@ -4,5 +4,9 @@ set -x
 set -e
 
 echo "hi from whatlist-cron"
+echo "attempt 1"
 wget --timeout=120 -nv -O- http://superlist-backend.default.svc.cluster.local:3003/scheduled-messages/cron || true
-# sleep 5
+
+sleep 30
+echo "attempt 2"
+wget --timeout=120 -nv -O- http://superlist-backend.default.svc.cluster.local:3003/scheduled-messages/cron || true
